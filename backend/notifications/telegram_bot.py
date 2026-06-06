5f518a3d3064cd15fea0110afeecbc2851b7be79
frtipan <frtipan1@espe.edu.ec>
2026-06-05 19:20:02 -0500

    Fix Telegram configuration

--- backend/notifications/telegram_bot.py
+++ backend/notifications/telegram_bot.py
@@ -11,9 +11,6 @@ CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_message(message):
 
-    print("TOKEN:", TOKEN)
-    print("CHAT_ID:", CHAT_ID)
-
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
     response = requests.post(
@@ -24,10 +21,5 @@ def send_message(message):
         },
         timeout=10
     )
-
-    print(response.status_code)
-    print(response.text)
-
-    response.raise_for_status()
 
     return response
